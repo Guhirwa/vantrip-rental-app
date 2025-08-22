11d2e9ed84f0bd9e4551c2d70ad5032be732d748
Guhirwa <guhirwachristian004@gmail.com>
2025-08-22 12:25:39 +0200

    Working On Home page - I used Tailwindcss to organize and style the UI for the Home page while maintaing the navigation routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Vans from "./assets/components/pages/Vans"
 import Home from "./assets/components/pages/Home"
 
 const App = () => {
-  const hoverHeaderElement = "transition duration-300 ease-in-out hover:text-orange-600 hover:scale-115"
+  const hoverHeaderElement = "transition duration-300 ease-in-out hover:text-[#d4781d] hover:scale-115"
   return (
     <>
       <header className = "flex justify-between mx-32 my-24 text-2xl font-bold">
--- src/assets/components/pages/Home.jsx
+++ src/assets/components/pages/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <h1>This is Home page</h1>
+    <div className = "flex flex-col text-white bg-[#909090] mx-1 rounded-md px-10 py-20 ">
+      <h1 className = "text-5xl font-bold">You got the travel plans, We got the travel vans.</h1>
+      <p className = "text-2xl font-extralight my-10">Add adventure to your life by joining the #VANLIFE movement. Rent the perfect van to make your perfect road trip.</p>
+      <Link className = "w-[20%] py-1 hover:scale-108 hover:bg-[#9e5915] transition ease-in-out duration-300 text-center mx-auto rounded-md font-bold text-xl bg-[#d4781d]" to = "/vans">Find your van</Link>
+    </div>
   )
 }
 
